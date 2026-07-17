ecc58bb9250d3a089ea4e008931fc774d4c49666
Ayodele Lawal <ayodanice1@gmail.com>
2026-07-17 10:09:20 +0100

    Initial React + Tailwind setup

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/',  // added for GitHub Pages. Set to '/' because repo is exactly blessed-ayo.github.io
+})
